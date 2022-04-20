4cce08c9faca6052816b9ff90a218cfb5aac928a
Bjoern Ebbrecht <b.ebbrecht@rl-3.de>
2022-04-20 23:28:24 +0200

    added new features

--- src/index.ts
+++ src/index.ts
@@ -2,10 +2,14 @@ import { Position } from './position';
 import { bearingTo } from './geomath/bearingTo';
 import { distance } from './geomath/distance';
 import { placeBearingDistance } from './geomath/placeBearingDistance';
+import { projectTurnPosition } from './geomath/projectTurnPosition';
+import { turnRadius } from './geomath/turnRadius';
 
 export {
     Position,
     bearingTo,
     distance,
-    placeBearingDistance
+    placeBearingDistance,
+    projectTurnPosition,
+    turnRadius
 };
--- src/position.ts
+++ src/position.ts
@@ -1,3 +1,5 @@
+// see http://www.c-dev.ch/2012/10/26/koordinatenformate/ for formatting
+
 export class Position {
     private _lat: number;
     private _lon: number;
@@ -8,13 +10,153 @@ export class Position {
         this._lon = lon;
     }
 
-    public get lat()
+    public get lat(): number
     {
         return this._lat;
     }
 
-    public get lon()
+    public get lon(): number
     {
         return this._lon;
+    }
+
+    public get latDegrees(): number
+    {
+        return Math.trunc(this._lat);
+    }
+
+    public get lonDegrees(): number
+    {
+        return Math.trunc(this._lon);
+    }
+
+    public get latDegreesAbs(): number
+    {
+        return Math.abs(this.latDegrees);
+    }
+
+    public get lonDegreesAbs(): number
+    {
+        return Math.abs(this.lonDegrees);
+    }
+
+    public get latMinutes(): number
+    {
+        return (this.lat % 1) * 60;
+    }
+
+    public get lonMinutes(): number
+    {
+        return (this.lon % 1) * 60;
+    }
+
+    public get latSeconds(): number
+    {
+        return (this.latMinutes % 1) * 60;
+    }
+
+    public get lonSeconds(): number
+    {
+        return (this.lonMinutes % 1) * 60;
+    }
+
+    public get latHemisphere(): string
+    {
+        if(this.isNorthernHemisphere()) {
+            return "N"
+        }
+        
+        return "S";
+    }
+
+    public get lonHemisphere(): string
+    {
+        if(this.isEasternHemisphere()) {
+            return "E"
+        }
+        
+        return "W";
+    }
+
+    public isNorthernHemisphere(): boolean
+    {
+        if(this._lat >= 0) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    public isSouthernHemisphere(): boolean
+    {
+        return !this.isNorthernHemisphere();
+    }
+
+    public isEasternHemisphere(): boolean
+    {
+        if(this._lon >= 0) {
+            return true;
+        }
+        
+        return false;
+    }
+
+    public isWesternHemisphere(): boolean
+    {
+        return !this.isEasternHemisphere();
+    }
+
+    public toDMS() {
+        let result = [];
+        result.push(this.latDegreesAbs.toString().padStart(2, '0')+"°");
+        result.push(Math.trunc(this.latMinutes).toString().padStart(2, '0')+"′");
+        result.push(this.latSeconds.toFixed(2).toString().padStart(2, '0')+"″");
+        result.push(this.latHemisphere);
+        
+        result.push(this.lonDegreesAbs.toString().padStart(2, '0')+"°");
+        result.push(Math.trunc(this.lonMinutes).toString().padStart(2, '0')+"′");
+        result.push(this.lonSeconds.toFixed(2).toString().padStart(2, '0')+"″");
+        result.push(this.lonHemisphere);
+
+        return result.join(" ");
+    }
+
+    public toDMSCode() {
+        let result = [];
+        result.push(this.latDegreesAbs.toString().padStart(2, '0'));
+        result.push(Math.trunc(this.latMinutes).toString().padStart(2, '0'));
+        result.push(Math.trunc(this.latSeconds).toString().padStart(2, '0'));
+        result.push(this.latHemisphere);
+        
+        result.push(this.lonDegreesAbs.toString().padStart(2, '0'));
+        result.push(Math.trunc(this.lonMinutes).toString().padStart(2, '0'));
+        result.push(Math.trunc(this.lonSeconds).toString().padStart(2, '0'));
+        result.push(this.lonHemisphere);
+
+        return result.join("");
+    }
+    
+    public toDMM() {
+        let result = [];
+        result.push(this.latDegreesAbs.toString().padStart(2, '0')+"°");
+        result.push(this.latMinutes.toFixed(3).toString().padStart(2, '0')+"′");
+        result.push(this.latHemisphere);
+        
+        result.push(this.lonDegreesAbs.toString().padStart(2, '0')+"°");
+        result.push(this.lonMinutes.toFixed(3).toString().padStart(2, '0')+"′");
+        result.push(this.lonHemisphere);
+
+        return result.join(" ");
+    }
+    
+    public toDDD() {
+        let result = [];
+        result.push(Math.abs(this.lat).toFixed(4).toString().padStart(7, '0')+"°");
+        result.push(this.latHemisphere);
+        
+        result.push(Math.abs(this.lon).toFixed(4).toString().padStart(8, '0')+"°");
+        result.push(this.lonHemisphere);
+
+        return result.join(" ");
     }
 }
