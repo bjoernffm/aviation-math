9d7da10ca631acead61dbf2c0f570a94621755c8
Bjoern Ebbrecht <b.ebbrecht@rl-3.de>
2023-04-03 04:00:19 +0200

    light changes, new docs

--- Gruntfile.js
+++ Gruntfile.js
@@ -4,7 +4,7 @@ module.exports = function(grunt) {
     grunt.initConfig({
         pkg: grunt.file.readJSON('package.json'),
         gitclone: {
-            pages: {
+            github_pages: {
                 options: {
                     repository: 'https://github.com/bjoernffm/bjoernffm.github.io',
                     branch: 'master',
@@ -12,13 +12,64 @@ module.exports = function(grunt) {
                 }
             }
         },
+        gitadd: {
+            github_pages: {
+                options: {
+                    cwd: "tmp_pages"
+                },
+                files: {
+                    src: '.'
+                }
+            }
+        },
+        gitcommit: {
+            github_pages: {
+                options: {
+                    cwd: "tmp_pages",
+                    message: 'Automated commit for publishing aviation-math docs'
+                },
+                files: {
+                    // Specify the files you want to commit
+                }
+            }
+        },
+        gitpush: {
+            github_pages: {
+                options: {
+                    cwd: "tmp_pages",
+                    // Target-specific options go here.
+                }
+            }
+        },
+        clean: {
+            github_pages: ['tmp_pages'],
+            docs: ['tmp_pages/aviation-math']
+        },
+        copy: {
+            docs: {
+                expand: true,
+                src: 'docs/**',
+                dest: 'tmp_pages/aviation-math/',
+            },
+        },
     });
 
     grunt.loadNpmTasks('grunt-git');
     grunt.loadNpmTasks('grunt-contrib-clean');
+    grunt.loadNpmTasks('grunt-contrib-copy');
+    grunt.loadNpmTasks('grunt-run');
   
     // Default task(s).
-    grunt.registerTask('default', ['gitclone:pages']);
-    grunt.registerTask('upload-docs', ['gitclone:pages']);
+    //grunt.registerTask('default', ['gitclone:docs']);
+    grunt.registerTask('upload-docs', [
+        'clean:github_pages',
+        'gitclone:github_pages',
+        'clean:docs',
+        'copy:docs',
+        'gitadd:github_pages',
+        'gitcommit:github_pages',
+        'gitpush:github_pages',
+        'clean:github_pages'
+    ]);
   
   };
